5446e30ba7800604a46905616b3b30c5af95775b
audeen <jannik.sievert@gmail.com>
2019-11-28 22:26:54 +0100

    billing läuft

--- root/intern/sites/billing.php
+++ root/intern/sites/billing.php
@@ -13,31 +13,9 @@
 session_start();
 
 include('../../config/config.php');
-include($lang_order_show);
+include($lang_bill);
 
-?>
-<!-- Include Security-File -->
-<?php include ('../../config/functions/authentification.php'); ?>
 
-<!DOCTYPE html>
-<html lang="de">
-  <!-- html-head einbinden -->
-  <?php include ('../../config/navigation/html_head.php'); ?>
-  <body>
-    <!-- backend-navigation einbinden -->
-    <?php include ('../../config/navigation/html_nav_be.php'); ?>
-
-    <div class="container">
-      <div class="container-fluid">
-        <div class="row">
-         <div class="alert alert-primary mt-3 col-12" role="alert">
-         <h2 class="text-center"><?php echo $lang_ordershow[$_SESSION['language']][0];?></h2>
-         <h3><?php echo $name = "" ?></h3>
-          </div>
-    <?php
-    // Sortierungsmöglichkeit im Table-Head über GET
-$sort = isset($_GET['sort']) ? (($_GET['sort'] === "DESC") ? "ASC" : "DESC") : "ASC"; //by rebitz
-$attribute = isset($_GET['attribute']) ? $_GET['attribute'] : "id_o";
 
 $retailer = "=".$_POST['retailer'];
 $year = $_POST['year'];
@@ -50,6 +28,38 @@ if ($month1 == "all") {
 if ($retailer == "=all") {
     $retailer = "LIKE '%'";
 }
+
+// Gesamtsumme Bestellungen/Monat errechnen & in Variable schreiben
+$sql =   $pdo->prepare("SELECT SUM(total)
+      FROM products, retailer, orders 
+      WHERE id_r $retailer
+      AND r_id = id_r
+      AND id_p = p_id
+      AND order_date BETWEEN '$year-$month1-01' AND '$year-$month2-31'");
+
+$sql->execute();
+$row = $sql->fetch();
+$total = $row['SUM(total)'];
+
+// Bonus berechnen
+
+if ($total > 0 and $total <= 1000){
+   $bonus = 500;
+}
+elseif ($total > 1000 and $total <= 3000){
+   $bonus = 1000;
+}
+elseif ($total > 3000){ 
+   $bonus = 1500;
+}
+else {
+   $bonus = 0;
+}
+
+
+
+
+// Daten aus Datenbank holen 
 $sql =   $pdo->prepare("SELECT * 
             FROM products, retailer, orders 
             WHERE id_r $retailer
@@ -57,45 +67,147 @@ $sql =   $pdo->prepare("SELECT *
             AND id_p = p_id
             AND order_date BETWEEN '$year-$month1-01' AND '$year-$month2-31'");
 $sql->execute();
-$row = $sql->fetchAll();
+$row = $sql->fetch();
 print_r($row);
-   
+
+$time = strtotime($row['order_date']);
+$billmonth = date('m', $time);
+$pay = $bonus + $row['basic_pay'];
 
 
-echo "<div id=\"orders\" class=\"table-responsive table-hover\">\n";
-echo "<table class=\"table\">";
-   echo "<thead>";
-      echo "<tr>";
-         echo "<th scope=\"col\"><a href=\"../../intern/sites/order_show.php?attribute=id_o&sort=".$sort."\">ID</a></th>";
-         echo "<th scope=\"col\"><a href=\"../../intern/sites/order_show.php?attribute=p_name&sort=".$sort."\">".$lang_ordershow[$_SESSION['language']][3]."</th>";
-         echo "<th scope=\"col\"><a href=\"../../intern/sites/order_show.php?attribute=order_date&sort=".$sort."\">".$lang_ordershow[$_SESSION['language']][4]."</th>";
-         echo "<th scope=\"col\"><a href=\"../../intern/sites/order_show.php?attribute=qty&sort=".$sort."\">".$lang_ordershow[$_SESSION['language']][5]."</th>";
-         echo "<th scope=\"col\">".$lang_ordershow[$_SESSION['language']][6]."</th>";
-      echo "</tr>";
-   echo "</thead>";
-   echo "<tbody";
-   $sql =   ("SELECT * FROM products, retailer, orders 
-            WHERE id_r $retailer
-            AND r_id = id_r
-            AND id_p = p_id
-            AND order_date BETWEEN '$year-$month1-01' AND '$year-$month2-31'");
+switch ($billmonth){
+   case "1":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][1];
+   break;
+   case "2":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][2];
+   break;
+   case "3":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][3];
+   break;
+   case "4":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][4];
+   break;
+   case "5":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][5];
+   break;
+   case "6":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][6];
+   break;
+   case "7":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][7];
+   break;
+   case "8":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][8];
+   break;
+   case "9":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][9];
+   break;
+   case "10":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][10];
+   break;
+   case "11":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][11];
+   break;
+   case "12":
+      $billmonth = $lang_billcalendar[$_SESSION['language']][12];
+   break;
+
+}
+
+
+
+?>
+<!-- Include Security-File -->
+<?php include ('../../config/functions/authentification.php'); ?>
+
+<!DOCTYPE html>
+<html lang="de">
+  <!-- html-head einbinden -->
+<?php include ('../../config/navigation/html_head.php'); ?>
+<body>
+<!-- backend-navigation einbinden -->
+<?php include ('../../config/navigation/html_nav_be.php'); ?>
+
+<div class="container">
+   <div class="container-fluid">
+      <div class="alert alert-primary mt-3 col-12" role="alert">
+         <h2 class="text-center"><?php echo $lang_billing[$_SESSION['language']][0];?></h2>
+      </div>
+
+
+<?php
+
+echo "<div>";
+   echo "<h4>Name: ".$row['r_prename']." ".$row['r_surname']."  </h4>";
+   echo "<h4>".$lang_billing[$_SESSION['language']][1].$billmonth." ".date("Y", $time)."</h4>";
+   echo "<h4>".$lang_billing[$_SESSION['language']][2].$billnumber=date("Y", $time)."/".date("m", $time)."/".$row['id_r']."</h4>";
+   echo "<br>";
+   echo "<br>";
+   echo "<h5>".$lang_billing[$_SESSION['language']][9]."</h5>";
+   echo "<p>".$row['r_prename']." ".$row['r_surname']."<br>".htmlentities($row['r_street'])."<br>".$row['r_postal']." ".$row['r_city']."<br>".$row['r_country']."</p>";
+   //Tabelle mit Bestellungen des jeweiligen Monats
+   echo "<h5>".$lang_billing[$_SESSION['language']][3]." ".$billmonth." ".date("Y", $time)."</h5>";
+   echo "<div id=\"orders\" class=\"table-responsive table-hover\">\n";
+      echo "<table class=\"table\">";
+         echo "<thead>";
+            echo "<tr>";
+               echo "<th scope=\"col\">Position</th>";
+               echo "<th scope=\"col\">".$lang_billing[$_SESSION['language']][4]."</th>";
+               echo "<th scope=\"col\">".$lang_billing[$_SESSION['language']][5]."</th>";
+               echo "<th scope=\"col\">".$lang_billing[$_SESSION['language']][6]."</th>";
+               echo "<th scope=\"col\">".$lang_billing[$_SESSION['language']][7]."</th>";
+            echo "</tr>";
+         echo "</thead>";
+         echo "<tbody";
+         //Ausgabe Bestellungen des Monats
+         $sql =   ("SELECT * FROM products, retailer, orders 
+                  WHERE id_r $retailer
+                  AND r_id = id_r
+                  AND id_p = p_id
+                  AND order_date BETWEEN '$year-$month1-01' AND '$year-$month2-31'");
 
          foreach ($pdo->query($sql) as $row) {
             echo "<tr>";
-            echo "<th scope=\"row\">".$row['id_o']."</th>";
-            echo "<td>".$row['p_name']."</td>";
-            echo "<td>".$row['order_date']."</td>";           
-            echo "<td>".$row['qty']." ".$lang_ordershow[$_SESSION['language']][7]."</td>";
-            echo "<td>".$row['qty']*$row['p_price']." &euro;</td>";
+               echo "<th scope=\"row\">DUMMY</th>";
+               echo "<td>".$row['p_name']."</td>";
+               echo "<td>".$row['qty']." ".$lang_billing[$_SESSION['language']][8]."</td>";
+               echo "<td>".$row['p_price']." &euro;</td>";
+               echo "<td>".$row['qty']*$row['p_price']." &euro;</td>";
             echo "</tr>";
-            
-         }
-   echo "</tbody";
+               }
+         echo "</tbody>";
+      echo "</table>";
+      echo "<div>";
+      echo "<div>";
+      echo "<table class=\"table\">";
+         echo "<tbody>";
+            echo "<tr>";
+               echo "<td>".$lang_billing[$_SESSION['language']][10]."</td>";
+               echo "<td>".$total." &euro;</td>";
+            echo "</tr>";
+            echo "<tr>";
+               echo "<td>".$lang_billing[$_SESSION['language']][11]."</td>";
+               echo "<td>".$row['basic_pay']." &euro;</td>";
+            echo "</tr>";
+            echo "<tr>";
+               echo "<td>".$lang_billing[$_SESSION['language']][12]."</td>";
+               echo "<td>".$bonus." &euro;</td>";
+            echo "</tr>";
+            echo "<tr>";
+               echo "<td>".$lang_billing[$_SESSION['language']][13]."</td>";
+               echo "<td>".$pay." &euro;</td>";
+            echo "</tr>";
+      echo "</table>";
+      echo "</div>";
+      echo "</div>";
+   echo "</div>";
 
-echo "</table>";
 echo "</div>";
 
+
+
 ?>
-          </div>
+
         </div>
       </div>
---
+++ root/language/billing.php
@@ -0,0 +1,63 @@
+<?php
+
+$lang_billing[0][0] = "Monatsabrechnung";
+$lang_billing[0][1] = "Monat: ";
+$lang_billing[0][2] = "Abrechnungsnummer: ";
+$lang_billing[0][3] = "Bestellungen ";
+$lang_billing[0][4] = "Produkt";
+$lang_billing[0][5] = "Menge";
+$lang_billing[0][6] = "Einzelpreis";
+$lang_billing[0][7] = "Summe";
+$lang_billing[0][8] = "St&uuml;ck";
+$lang_billing[0][9] = "Rechnungsanschrift:";
+$lang_billing[0][10] = "Gesamtsumme Bestellungen:";
+$lang_billing[0][11] = "Grundgehalt:";
+$lang_billing[0][12] = "Bonus:";
+$lang_billing[0][13] = "Gesamtgehalt";
+$lang_billing[0][8] = "St&uuml;ck";
+
+
+
+$lang_billing[1][0] = "Monthly Sale Slip";
+$lang_billing[1][1] = "Month: ";
+$lang_billing[1][2] = "Billing Number: ";
+$lang_billing[1][3] = "Orders ";
+$lang_billing[1][4] = "Product";
+$lang_billing[1][5] = "Quantity";
+$lang_billing[1][6] = "Price per unit";
+$lang_billing[1][7] = "Sum";
+$lang_billing[1][8] = "pieces";
+
+$lang_billcalendar[0][0] = "";
+$lang_billcalendar[0][1] = "Januar";
+$lang_billcalendar[0][2] = "Februar";
+$lang_billcalendar[0][3] = "M&auml;rz";
+$lang_billcalendar[0][4] = "April";
+$lang_billcalendar[0][5] = "Mai";
+$lang_billcalendar[0][6] = "Juni";
+$lang_billcalendar[0][7] = "Juli";
+$lang_billcalendar[0][8] = "August";
+$lang_billcalendar[0][9] = "September";
+$lang_billcalendar[0][10] = "Oktober";
+$lang_billcalendar[0][11] = "November";
+$lang_billcalendar[0][12] = "Dezember";
+$lang_billcalendar[0][13] = "Ganzes Jahr";
+
+
+$lang_billcalendar[1][0] = "";
+$lang_billcalendar[1][1] = "January";
+$lang_billcalendar[1][2] = "February";
+$lang_billcalendar[1][3] = "March";
+$lang_billcalendar[1][4] = "April";
+$lang_billcalendar[1][5] = "May";
+$lang_billcalendar[1][6] = "June";
+$lang_billcalendar[1][7] = "July";
+$lang_billcalendar[1][8] = "August";
+$lang_billcalendar[1][9] = "September";
+$lang_billcalendar[1][10] = "October";
+$lang_billcalendar[1][11] = "November";
+$lang_billcalendar[1][12] = "December";
+$lang_billcalendar[1][13] = "Whole Year";
+
+
+?>
